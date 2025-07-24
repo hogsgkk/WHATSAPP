81146a30a61da2ad66e6aa17303fd589df923595
hogsgkk <hozu420@outlook.com>
2025-07-24 13:32:00 +0530

    Add express server and simple chat UI

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
